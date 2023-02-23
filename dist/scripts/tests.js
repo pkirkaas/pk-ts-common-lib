f76edd3c7f2d0b8c3ab7016d107ef1778aeed967
Paul Kirkaas <pkirkaas@gmail.com>
2023-02-23 21:04:23 +0100

    Testing export

--- dist/scripts/tests.js
+++ dist/scripts/tests.js
@@ -4,5 +4,7 @@ const __1 = require("..");
 let tobj = { a: 8 };
 let j5 = __1.JSON5.stringify(tobj);
 let to = (0, __1.typeOf)(tobj);
-console.log("Testing tests", { tobj, to, j5, jsdc: __1.jsdc });
+let toJ = (0, __1.typeOf)(JSON);
+let toJ5 = (0, __1.typeOf)(__1.JSON5);
+console.log("Testing tests", { tobj, to, j5, jsdc: __1.jsdc, toJ5, toJ });
 //# sourceMappingURL=tests.js.map
