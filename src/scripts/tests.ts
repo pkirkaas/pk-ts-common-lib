ee382eebb69e7e8acef9c2ec7f9026db566e5586
Paul Kirkaas <pkirkaas@gmail.com>
2023-02-23 21:04:09 +0100

    Testing export

--- src/scripts/tests.ts
+++ src/scripts/tests.ts
@@ -2,4 +2,6 @@ import { typeOf, isEmpty, jsdc, JSON5 } from '..';
 let tobj = { a: 8 };
 let j5 = JSON5.stringify(tobj);
 let to = typeOf(tobj);
-console.log("Testing tests", { tobj, to, j5, jsdc });
+let toJ = typeOf(JSON);
+let toJ5 = typeOf(JSON5);
+console.log("Testing tests", { tobj, to, j5, jsdc, toJ5, toJ });
