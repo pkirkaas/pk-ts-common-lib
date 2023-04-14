3560d45f5f0ab6558bcaa61d2e203a30ebd1c33d
Paul Kirkaas <pkirkaas@gmail.com>
2023-04-14 02:57:57 +0200

    About to actually try to support BOTH ESM & CommonJS

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,6 @@
 /** Init shared by MongoQP-api & MongoQP-client */
 export * from './lib/json-decycle.js';
-//export const axios = require('axios');
-//export const axios = require('axios');
 import axios from 'axios';
-//export * from 'axios';
-//import jsondecycle from 'json-decycle';
-//export * from 'json-decycle';
-//export { jsondecycle };
-//export const JSON5 = require('json5');
-
-//export JSON5 from 'json5';
-
-
-/*
-import JSON5 from 'json5';
-export JSON5;
-*/
-//export { axios, jsondecycle };
-//require("json-decycle").extend(JSON5);
-//const jsondecycle = require("json-decycle");
-/*
-import { extend } from "json-decycle";
-extend(JSON5);
-*/
 export type OptArrStr = string | string[];
 export type Falsy = false | 0 | "" | null | undefined;
 export type GenericObject = { [key: string]: any };
